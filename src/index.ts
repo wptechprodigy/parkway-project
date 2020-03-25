deb408ce8ab2f70df9d29058561ac6aa80e06681
wptechprodigy <waheed.freelancer16@gmail.com>
2020-03-25 17:33:57 +0100

    add check for amount below 0

--- src/index.ts
+++ src/index.ts
@@ -11,23 +11,25 @@ const calculateButton = document.getElementById('main__submitbutton');
 
 if (calculateButton) {
 	calculateButton.addEventListener('click', () => {
-    const amount: number = parseFloat(transactionInput.value);
+		const amount: number = parseFloat(transactionInput.value);
 
 		if (isNaN(amount) || amount < 1) {
-      alert('Please provide a valid transfer amount');
+			alert('Please provide a valid transfer amount');
 
-      transactionInput.value = '';
+			transactionInput.value = '';
 
-      if (displayArea) displayArea.innerText = '';
-      return;
-    }
+			if (displayArea) displayArea.innerText = '';
+			return;
+		}
 
 		if (displayArea) {
-      displayArea.innerText = `The transfer fees for NGN${amount} is NGN${String(calculateFees.getTransferFees(amount))}`;
+			displayArea.innerText = `The transfer fees for NGN${amount} is NGN${String(
+				calculateFees.getTransferFees(amount),
+			)}`;
 
-      transactionInput.value = '';
+			transactionInput.value = '';
 		}
-  });
+	});
 
-  transactionInput.value = '';
+	transactionInput.value = '';
 }
